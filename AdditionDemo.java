7361fe01453c0075f8fb3c121dafe0f151ee2d8b
Tushar Shinde <134844370+tusharshinde0301@users.noreply.github.com>
2023-09-28 12:09:28 +0530

    Add files via upload

---
+++ AdditionDemo.java
@@ -0,0 +1,11 @@
+package project1;
+
+public class AdditionDemo {
+
+	protected void add(int i,int j)
+	{
+		System.out.println(i+j);
+
+	}
+
+}
---
+++ Demo1.java
@@ -0,0 +1,20 @@
+package project2;
+import project1.AdditionDemo;
+
+class kp extends AdditionDemo
+{
+	void show()
+	{
+		add(10,5);
+	}
+}
+public class Demo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		kp a1=new kp();
+		a1.show();
+
+	}
+
+}
